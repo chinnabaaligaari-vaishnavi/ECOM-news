638298021d26b95197b3a1b755152dc5b4002d89
CHINNABAALIGAARI VAISHNAVI <162132723+chinnabaaligaari-vaishnavi@users.noreply.github.com>
2025-11-14 11:42:39 +0530

    Update app.js

---
+++ project/backends/frontend/public/src/components/app.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ProductList from "./components/ProductList";
+import NewsFeed from "./components/NewsFeed";
+function App() {
+  return <div>
+    <ProductList />
+    <NewsFeed />
+  </div>;
+}
+export default App;
